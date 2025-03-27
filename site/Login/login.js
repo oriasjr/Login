9b0c7d8de9b97a241cf0e7e5f3c49240ebd26714
oriasjr <oriasjunior9@gmail.com>
2025-03-26 23:47:06 -0300

    Login pronto

---
+++ site/Login/login.js
@@ -0,0 +1,13 @@
+function login() {
+    const usuarioCorreto = "admin";
+    const senhaCorreta = "1234";
+
+    const usuario = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;
+
+    if (usuario === usuarioCorreto && senha === senhaCorreta) {
+        window.location.href = "/sucesso/inicial.html";
+    } else {
+        alert("Usuário ou senha incorretos!");
+    }
+}
